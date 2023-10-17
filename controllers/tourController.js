879861c03bb76bbddac209c1e7073be75f5a2183
boobaGreen <devboobagreen@gmail.com>
2023-10-18 01:15:42 +0200

    update 18-10-23 ver 3

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,18 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query }; // new hard copy
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const tours = await Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
     res.status(200).json({
       status: 'succes',
       results: tours.length,
