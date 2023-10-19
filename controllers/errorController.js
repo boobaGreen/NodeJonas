8de6e7f8fdcaa9c8f5cdfc9e4ba1e55f707fda53
boobaGreen <devboobagreen@gmail.com>
2023-10-20 00:58:48 +0200

    update 20-10-23 ver2

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,37 @@
 //Global Error Handling Middleware - 4 argument express recognize is a error middleware
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const sendErrorProduction = (err, res) => {
+  // Operational Error ,trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or othe unknown error : don't leak error details to client
+  } else {
+    //1) Log error to server console
+    console.error('ERROR 💥', err);
+    //2) Send generic message to client
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong !',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // internal server error 500
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res);
+  }
 };
