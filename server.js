e8a0138ef9ab6d04cea706585705b72ec0a1dc49
boobaGreen <devboobagreen@gmail.com>
2023-10-21 00:21:17 +0200

    update 21-10-23

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -27,7 +33,7 @@ const DB = process.env.DATABASE.replace(
 async function dbConnect() {
   await mongoose.connect(DB);
 }
-dbConnect().catch((err) => console.log(err));
+dbConnect();
 
 // console.log(app.get('env')); // express
 // console.log(process.env); //core module node
@@ -47,6 +53,14 @@ dbConnect().catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
